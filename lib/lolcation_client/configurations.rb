f3212bc3ee7a39d1d283c25190d90a89aee5b750
Eduardo Zaghi <eduardo@loldesign.com.br>
2017-07-04 14:59:02 -0300

    Update to right service path

--- lib/lolcation_client/configurations.rb
+++ lib/lolcation_client/configurations.rb
@@ -1,5 +1,5 @@
 module LolcationClient
   module Configurations
-    URL = "http://localhost:3000/api/v1/localizations"
+    URL = "https://lolcation-service.loldesign.com.br/api/v1/localizations"
   end
 end
