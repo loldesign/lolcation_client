fe0723acf56a6db69df8a56db8da52291202f2c9
Marcelo Barreto <marcelobarretojunior@gmail.com>
2017-07-04 17:43:43 -0300

    wip

--- lib/lolcation_client.rb
+++ lib/lolcation_client.rb
@@ -4,9 +4,11 @@ module LolcationClient
   autoload :Interceptor,      "lolcation_client/interceptor"
   autoload :Configurations,   "lolcation_client/configurations"
   autoload :CustomFields,     "lolcation_client/custom_fields"
+  autoload :NearIn,           "lolcation_client/near_in"
 
   def self.extended(base)
     base.include LolcationClient::Interceptor
     base.extend  LolcationClient::CustomFields
+    base.extend  LolcationClient::NearIn
   end
 end
---
+++ lib/lolcation_client/near_in.rb
@@ -0,0 +1,16 @@
+require "faraday"
+require_relative "configurations"
+
+module LolcationClient
+  module NearIn
+    def near_in(*params)
+
+    end
+
+    private
+
+    def grab_objects
+
+    end
+  end
+end
