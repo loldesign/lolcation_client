eaf89d6ef02f6f349f8bb0f01fde94be18b0d871
Eduardo Zaghi <eduardo@loldesign.com.br>
2017-08-01 15:08:45 -0300

    rfactor lolcation_fields

--- lib/lolcation_client/lolcation_fields.rb
+++ lib/lolcation_client/lolcation_fields.rb
@@ -3,8 +3,16 @@ module LolcationClient
     def lolcation_fields(options = {})
       self.class_eval do
         before_validation do
-          fields = [:latitude, :longitude, :name, :address_street, :address_neighborhood,
-            :address_city, :address_state, :address_number]
+          fields = [
+            :latitude,
+            :longitude,
+            :name,
+            :address_street,
+            :address_neighborhood,:address_city,
+            :address_state,
+            :address_number,
+            :address_zipcode,
+          ]
 
           fields.each do |field|
             self.send("lolcation_#{field}=", self.send(options[field] || "lolcation_#{field}"))
