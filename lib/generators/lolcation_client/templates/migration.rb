51155165aee57f7e6845995da888143905fdb909
Eduardo Zaghi <eduardo@loldesign.com.br>
2017-08-01 15:54:36 -0300

    add right field type to migration

--- lib/generators/lolcation_client/templates/migration.rb
+++ lib/generators/lolcation_client/templates/migration.rb
@@ -1,16 +1,16 @@
 class AddLolcationTo<%= table_name.camelize %> < ActiveRecord::Migration
   def self.up
     change_table :<%= table_name %> do |t|
-      t.float :lolcation_latitude
-      t.float :lolcation_longitude
-      t.string :lolcation_id
-      t.string :lolcation_name
-      t.string :lolcation_address_street
-      t.string :lolcation_address_neighborhood
-      t.string :lolcation_address_city
-      t.string :lolcation_address_state
-      t.string :lolcation_address_number
-      t.string :lolcation_address_zipcode
+      t.float   :lolcation_latitude
+      t.float   :lolcation_longitude
+      t.integer :lolcation_id
+      t.string  :lolcation_name
+      t.string  :lolcation_address_street
+      t.string  :lolcation_address_neighborhood
+      t.string  :lolcation_address_city
+      t.string  :lolcation_address_state
+      t.string  :lolcation_address_number
+      t.string  :lolcation_address_zipcode
     end
   end
 end
