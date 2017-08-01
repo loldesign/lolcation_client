865da0dfbe009f7384a32e954468160cb366d191
Eduardo Zaghi <eduardo@loldesign.com.br>
2017-08-01 15:09:14 -0300

    refactor interception

--- lib/lolcation_client/interceptor.rb
+++ lib/lolcation_client/interceptor.rb
@@ -7,22 +7,23 @@ module LolcationClient
 
     def self.included(model)
       model.send(:after_validation) do
-        if self.lolcation_id.present?
-          response = patch_on_lolcation_server
+        if self.persisted?
+          response = update_on_lolcation_server
         else
-          response = post_on_lolcation_server
+          response = create_on_lolcation_server
         end
 
-        parsed_response = JSON.parse(response.body)
-        if parsed_response["error"]
-          self.errors.add(:base, parsed_response["error"])
+        json = JSON.parse(response.body, object_class: OpenStruct)
+
+        if json.error.present?
+          self.errors.add(:base, json.error)
           false
-        elsif parsed_response['localization']
-          self.lolcation_id = parsed_response["localization"]["objectId"]
-          self.lolcation_latitude = parsed_response["localization"]["latitude"]
-          self.lolcation_longitude = parsed_response["localization"]["longitude"]
+        elsif json.localization.present?
+          self.lolcation_id = json.localization.id
+          self.lolcation_latitude = json.localization.latitude
+          self.lolcation_longitude = json.localization.longitude
         else
-          parsed_response.map {|error, message| self.errors.add("lolcation_#{error}", message[0])}
+          json.map {|error, message| self.errors.add("lolcation_#{error}", message[0])}
           false
         end
       end
@@ -34,9 +35,10 @@ module LolcationClient
       self.class.try(:lolcation_custom_fields) || []
     end
 
-    def set_custom_fields
+    def build_custom_fields
       attributes = custom_fields.map {|attribute| {attribute => self.try(attribute)}}
-      hash = {}
+      hash       = {}
+
       attributes.each do |attribute|
         attribute.each do |key, value|
           hash[key] = value
@@ -46,40 +48,41 @@ module LolcationClient
       hash
     end
 
-    def prepare_object_to_post
+    def build_params
       {
         localization: {
-          latitude: self.try(:lolcation_latitude),
-          longitude: self.try(:lolcation_longitude),
-          name: self.try(:lolcation_name),
-          address_street: self.try(:lolcation_address_street),
+          latitude:             self.try(:lolcation_latitude),
+          longitude:            self.try(:lolcation_longitude),
+          name:                 self.try(:lolcation_name),
+          address_street:       self.try(:lolcation_address_street),
           address_neighborhood: self.try(:lolcation_address_neighborhood),
-          address_city: self.try(:lolcation_address_city),
-          address_state: self.try(:lolcation_address_state),
-          address_number: self.try(:lolcation_address_number),
-          custom_fields: set_custom_fields
+          address_city:         self.try(:lolcation_address_city),
+          address_state:        self.try(:lolcation_address_state),
+          address_number:       self.try(:lolcation_address_number),
+          address_zipcode:      self.try(:lolcation_address_zipcode),
+          #custom_fields:        build_custom_fields
         },
         sandbox: sandbox?
-      }
+      }.to_json
     end
 
-    def post_on_lolcation_server
+    def create_on_lolcation_server
       url = LolcationClient::Configurations::URL
       conn = Faraday.new(url: url)
       conn.post do |r|
         r.headers["X-Token"] = token
         r.headers["Content-Type"] = "application/json"
-        r.body = prepare_object_to_post.to_json
+        r.body = build_params
       end
     end
 
-    def patch_on_lolcation_server
+    def update_on_lolcation_server
       url = LolcationClient::Configurations::URL
       conn = Faraday.new(url: "#{url}/#{self.lolcation_id}")
       conn.put do |r|
         r.headers["X-Token"] = token
         r.headers["Content-Type"] = "application/json"
-        r.body = prepare_object_to_post.to_json
+        r.body = build_params
       end
     end
   end
