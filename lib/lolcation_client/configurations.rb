194133f0d159de4c581e65f0888b4b4b8ad51884
Eduardo Zaghi <eduardo@loldesign.com.br>
2017-08-15 12:45:49 -0300

    fixing error at configuration

--- lib/lolcation_client/configurations.rb
+++ lib/lolcation_client/configurations.rb
@@ -1,9 +1,7 @@
 module LolcationClient
   module Configurations
     API = "/api/v1/localizations"
-    URL = Rails.env.development?
-            ? "http://localhost:8000#{API}" 
-            : "https://lolcation-service.loldesign.com.br#{API}"
+    URL = Rails.env.development? ? "http://localhost:8000#{API}" : "https://lolcation-service.loldesign.com.br#{API}"
 
     def configs
       Rails.application.config_for(:lolcation)
--- spec/dummy/app/models/place.rb
+++ spec/dummy/app/models/place.rb
@@ -3,3 +3,14 @@ class Place < ApplicationRecord
 
 	lolcation_custom_fields :active
 end
+
+
+# place = Place.create(
+# 	lolcation_name: 'caso do edu e da shi',
+# 	lolcation_address_street: 'rua dos alpes',
+# 	lolcation_address_neighborhood: 'cambuci',
+# 	lolcation_address_city: 'sao paulo',
+# 	lolcation_address_state: 'sp',
+# 	lolcation_address_number: '120',
+# 	lolcation_address_zipcode: '01520-030',
+# )
